935112e5a8ba100eea71b33b670a038b3ab87723
Jay Dizzle <pogiest1@yahoo.com>
2013-10-28 19:26:21 -0700

    implement forward and backward movement using pageControl

--- PageViewController/PVCPagesViewController.m
+++ PageViewController/PVCPagesViewController.m
@@ -24,6 +24,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    // instantiate the view controlles from the storyboard
     UIViewController *page1 = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"page1"];
 
     UIViewController *page2 = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"page2"];
@@ -32,6 +34,7 @@
 
     UIViewController *page4 = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"page4"];
 
+    // load the view controllers in our pages array
     self.pages = [[NSArray alloc] initWithObjects:page1, page2, page3, page4, nil];
 
     self.pageController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal options:nil];
@@ -40,15 +43,18 @@
 
     [[self.pageController view] setFrame:[[self view] bounds]];
     NSArray *viewControllers = [NSArray arrayWithObject:[self.pages objectAtIndex:0]];
-    [self.pageController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    [self.pageControl setCurrentPage:0];
+    [self.pageControl addTarget:self action:@selector(changePage:) forControlEvents:UIControlEventValueChanged];
 
+    
+    [self.pageController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    
     [self addChildViewController:self.pageController];
     
-    
     [self.view addSubview:self.pageControl];
-    
     [[self view] addSubview:[self.pageController view]];
     [self.pageController didMoveToParentViewController:self];
+    
     [self.view sendSubviewToBack:[self.pageController view]];
 }
 
@@ -60,50 +66,48 @@
 
 - (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
     
-    for (NSUInteger i = 0; i < self.pages.count; i++) {
-        if ([viewController isEqual:[self.pages objectAtIndex:i]]) {
-            if (i < [self.pages count]-1) {
-                return [self.pages objectAtIndex:i+1];
-            } else {
-                return nil;
-            }
-        }
-    }
-
-    return nil;
-}
-
-
-- (UIViewController *)
-pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
-
-    for (NSUInteger i = 0; i < self.pages.count; i++) {
-        if ([viewController isEqual:[self.pages objectAtIndex:i]]) {
-            if (i > 0) {
-                return [self.pages objectAtIndex:i-1];
-            } else {
-                return nil;
-            }
-        }
-    }
+    NSUInteger currentIndex = [self.pages indexOfObject:viewController];    // get the index of the current view controller on display
+    [self.pageControl setCurrentPage:self.pageControl.currentPage+1];                   // move the pageControl indicator to the next page
     
-    return nil;
-}
-
-- (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController {
-    return [self.pages count];
-}
-
-- (NSInteger)presentationIndexForPageViewController:(UIPageViewController *)pageViewController {
-    PVCPagesViewController *currentController = (PVCPagesViewController *)pageViewController.viewControllers[0];
-    
-    for (NSUInteger i = 0; i < self.pages.count; i++) {
-        if ([currentController isEqual:[self.pages objectAtIndex:i]]) {
-            return i;
-        }
+    // check if we are at the end and decide if we need to present the next viewcontroller
+    if ( currentIndex < [self.pages count]-1) {
+        return [self.pages objectAtIndex:currentIndex+1];                   // return the next view controller
+    } else {
+        return nil;                                                         // do nothing
     }
-    
-    return 0;
 }
+
+
+- (UIViewController *) pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+    
+    NSUInteger currentIndex = [self.pages indexOfObject:viewController];    // get the index of the current view controller on display
+    [self.pageControl setCurrentPage:self.pageControl.currentPage-1];                   // move the pageControl indicator to the next page
+
+    // check if we are at the beginning and decide if we need to present the previous viewcontroller
+    if ( currentIndex > 0) {
+        return [self.pages objectAtIndex:currentIndex-1];                   // return the previous viewcontroller
+    } else {
+        return nil;                                                         // do nothing
+    }
+}
+
+- (void)changePage:(id)sender {
+    
+    UIViewController *visibleViewController = self.pageController.viewControllers[0];
+    NSUInteger currentIndex = [self.pages indexOfObject:visibleViewController];
+    
+    NSArray *viewControllers = [NSArray arrayWithObject:[self.pages objectAtIndex:self.pageControl.currentPage]];
+    
+    if (self.pageControl.currentPage > currentIndex) {
+        [self.pageController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionForward animated:NO completion:nil];
+    } else {
+        [self.pageController setViewControllers:viewControllers direction:UIPageViewControllerNavigationDirectionReverse animated:NO completion:nil];
+        
+    }
+}
+
+
+
+
 
 @end
